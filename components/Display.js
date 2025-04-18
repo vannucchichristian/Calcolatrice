adfd17521f6c0bb0180c4cc34f262629db4c9280
brunganiccolo <brunga.niccolo@studenti-ittfedifermi.edu.it>
2025-04-18 16:57:20 +0200

    Aggiunta funzione scroll al display

--- components/Display.js
+++ components/Display.js
@@ -1,12 +1,47 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useRef, useEffect } from 'react';
+import { View, Text, ScrollView } from 'react-native';
 import styles from '../styles/style';
 
 export default function Display({ input, output }) {
+    const inputScrollRef = useRef(null);  // Gestisce lo scorrimento automatico dell'area di input
+
+    useEffect(() => {
+        if (inputScrollRef.current) {
+            inputScrollRef.current.scrollToEnd({ animated: true });  // Scorre automaticamente all'ultima riga ogni volta che la precedente viene riempita
+        }
+    }, [input]);
+
     return (
         <View style={styles.display}>
-            <Text style={styles.inputText}>{input}</Text>
-            <Text style={styles.outputText}>{output}</Text>
+            <View>
+                {/* Visualizza i testi in un'area "scrollabile" */}
+                <ScrollView
+                    ref={inputScrollRef}
+                    style={{
+                        maxHeight: 150,
+                        alignSelf: 'stretch',
+                    }}
+                    contentContainerStyle={{ paddingVertical: 4 }}
+                ><Text
+                        style={[
+                            styles.inputText,
+                            { textAlign: 'right' },
+                        ]}
+                    >{input}
+                    </Text>
+                </ScrollView>
+                <ScrollView
+                    style={{ maxHeight: 60 }}
+                    contentContainerStyle={{ paddingVertical: 4 }}
+                ><Text
+                        style={[
+                            styles.outputText,
+                            { textAlign: 'right' },
+                        ]}
+                    >{output}
+                    </Text>
+                </ScrollView>
+            </View>
         </View>
     );
 };
